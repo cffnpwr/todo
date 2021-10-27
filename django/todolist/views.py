714b76b117be0266e810349e0454dd566d9a7de6
Caffeine Power <yuto1001okuda@gmail.com>
2021-10-27 15:59:51 +0900

    自身のデータのみを操作できるようにした

--- django/todolist/views.py
+++ django/todolist/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status, viewsets, permissions
-from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
+
+from collections import OrderedDict
 
 from .models import ToDoList
 from .serializer import ToDoSerializer
@@ -23,4 +23,41 @@ class ToDoViewSet(viewsets.ModelViewSet):
         return Response(toDoSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request, *args, **kwargs):
-        return Response(data=request.todolist, status=status.HTTP_200_OK)
+        queryset = self.filter_queryset(self.get_queryset())
+        toDoSerializer = self.get_serializer(queryset, many=True)
+
+        outputData = []
+        for data in toDoSerializer.data :
+            if data['user'] == request.user.username :
+                outputData.append(data)
+
+        return Response(outputData, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        if serializer.data['user'] == request.user.username :    
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if self.get_serializer(instance).data['user'] == request.user.username :
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_200_OK)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if self.get_serializer(instance).data['user'] == request.user.username :
+            toDoSerializer = self.get_serializer(instance, data=request.data)
+            toDoSerializer.is_valid(raise_exception=True)
+            self.perform_update(toDoSerializer)
+
+            return Response(toDoSerializer.data, status=status.HTTP_200_OK)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
